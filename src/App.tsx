7354e52679ecbb04ae3a31a011a343590577a8a9
Gustavo Rosolen Brigatti <gustavo.brigatti13@gmail.com>
2022-03-31 20:34:13 -0300

    criação de array com as informações da lista

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
+import { useState } from 'react';
 import * as C from './App.styles';
+import { Item } from './types/Item';
 
 const App = () => {
+  const [list, setList] = useState<Item[]>([
+    {id: 1,name: 'Comprar o pão na padaria',done: false},
+    {id: 2,name: 'Comprar um bolo na padaria',done: false},
+  ]);
+
   return (
       <C.Container>
         <C.Area>
-          <C.Header>
-              Lista de Tarefas
-          </C.Header>
+          <C.Header>Lista de Tarefas</C.Header>
+
+          {list.map((item, index)=>(
+              <div>{item.name}</div>
+          ))}
         </C.Area>
       </C.Container>
   );
